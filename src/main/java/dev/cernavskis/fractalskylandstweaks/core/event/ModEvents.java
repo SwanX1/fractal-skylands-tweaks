8dde32260868120a2f78e0123b04f84df1382b5c
Kārlis Čerņavskis <karliscern@gmail.com>
2024-04-17 23:10:21 +0300

    unfractaled

--- src/main/java/dev/cernavskis/fractalskylandstweaks/core/event/ModEvents.java
+++ src/main/java/dev/cernavskis/fractalskylandstweaks/core/event/ModEvents.java
@@ -1,7 +1,6 @@
 // Copyright (c) 2024 Kārlis Čerņavskis, All Rights Reserved unless otherwise explicitly stated.
 package dev.cernavskis.fractalskylandstweaks.core.event;
 
-import dev.cernavskis.fractalskylandstweaks.core.FSTChunkGenerators;
 import dev.cernavskis.fractalskylandstweaks.core.FSTLootConditions;
 import dev.cernavskis.fractalskylandstweaks.integrations.ftbquests.FSTTaskTypes;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -9,7 +8,6 @@ import net.minecraftforge.eventbus.api.IEventBus;
 public class ModEvents extends EventListener {
   @Override
   public void onModInitialization(IEventBus modEventBus) {
-    FSTChunkGenerators.registerToBus(modEventBus);
     FSTLootConditions.registerToBus(modEventBus);
 
     // Classload FTBQuest custom tasks
--- src/main/java/dev/cernavskis/fractalskylandstweaks/world/gen/islands/IslandConstructor.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024 Kārlis Čerņavskis, All Rights Reserved unless otherwise explicitly stated.
-package dev.cernavskis.fractalskylandstweaks.world.gen.islands;
-
-import net.minecraft.core.BlockPos;
-
-@FunctionalInterface
-public interface IslandConstructor {
-  Island create(IslandConfiguration config, BlockPos position, long seed);
-}
